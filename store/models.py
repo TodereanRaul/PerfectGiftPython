3941902ba3661f0b3b48159fc6232956427b7e89
Toderean R. <raultoderean20@gmail.com>
2025-01-29 20:19:14 +0100

    Created app store and products models

---
+++ store/models.py
@@ -0,0 +1,21 @@
+from django.contrib.postgres.fields import ArrayField
+from django.db import models
+
+# Create your models here.
+"""
+Product
+- nom
+- prix
+- available
+- description
+- image
+- autres variantes
+"""
+class Product (models.Model):
+    name = models.CharField(max_length=200)
+    price = models.FloatField(default=0.0)
+    available = models.BooleanField(default=True)
+    description = models.TextField(blank=True) #Allow admin to register items without description
+    thumbnail = models.ImageField(upload_to="products", blank=True, null=True) #Stores Images in folder named 'products'
+    variantIds = ArrayField(models.IntegerField(), blank=True)
+
