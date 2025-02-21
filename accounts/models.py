84f6def628a909a338836f4753ff64980bf81634
Toderean R. <raultoderean20@gmail.com>
2025-02-21 13:57:28 +0100

    created a personal user gestionnaire

--- accounts/models.py
+++ accounts/models.py
@@ -1,5 +1,26 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.contrib.auth.models import BaseUserManager
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, password, **kwargs):
+        if not email:
+            raise ValueError("Email is required")
+        
+        email = self.normalize_email(email)
+        user = self.model(email=email, **kwargs)
+        user.set_password(password) #encrypt password
+        user.save()
+        return user
+
+    def create_superuser(self, email, password, **kwargs):
+        user = self.create_user(email, password, **kwargs)
+        user.is_staff = True
+        user.is_superuser = True
+        user.is_active = True
+        user.save()
+        return user
+
 
 class Shopper(AbstractUser):
     username = None
@@ -8,3 +29,4 @@ class Shopper(AbstractUser):
     USERNAME_FIELD = 'email'
     # mandatrory fields for login
     REQUIRED_FIELDS = []
+    objects = CustomUserManager()
