b2b82d28ee0bd1b9eb1e470dea366d7b20eab6d8
Toderean R. <raultoderean20@gmail.com>
2025-03-07 20:32:49 +0100

    fixed translations issues - add contact form/html

--- shop/settings.py
+++ shop/settings.py
@@ -131,9 +131,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
 
-LANGUAGE_CODE = 'fr-fr'
-
-MODELTRANSLATION_DEFAULT_LANGUAGE = 'fr'
+LANGUAGE_CODE = 'fr'
 
 LANGUAGES = [
     ('en', _('English')),
--- store/views.py
+++ store/views.py
@@ -26,6 +26,9 @@ def index(request):
 def about(request):
     return render(request, 'store/about.html')
 
+def contact(request):
+    return render(request, 'store/contact.html')
+
 def product_list(request):
     products = Product.objects.all()
     return render(request, 'store/product-list.html', context={"products": products})
