611719c54cc504e726c8c5df8f7c1824dff9310d
TodereanRaul <raultoderean20@gmail.com>
2025-04-03 10:48:38 +0200

    modify models upload and media root

--- store/models.py
+++ store/models.py
@@ -18,7 +18,7 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
     available = models.BooleanField(default=True)
     description = RichTextField(blank=True)  # RichText pour mise en forme
-    thumbnail = models.ImageField(upload_to="products", blank=True, null=True)  # Image principale
+    thumbnail = models.ImageField(upload_to="products/", blank=True, null=True)  # Image principale
     variants = models.ManyToManyField("self", blank=True, symmetrical=False)
     # user_comment = models.TextField(blank=True, null=True)
     stripe_id = models.CharField(max_length=90, blank=True)
@@ -36,7 +36,7 @@ class Product(models.Model):
 
 class ProductImage(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name="images")
-    image = models.ImageField(upload_to="products", blank=True, null=True)
+    image = models.ImageField(upload_to="products/", blank=True, null=True)
 
     def __str__(self):
         return f"Image de {self.product.name}"
