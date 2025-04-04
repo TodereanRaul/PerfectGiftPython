e866ce55338d0812ee73e26352b7ae01313d37b5
Toderean R. <raultoderean20@gmail.com>
2025-04-04 13:49:56 +0200

    add sitemap

--- shop/urls.py
+++ shop/urls.py
@@ -3,6 +3,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.views.static import serve
+from django.views.generic.base import TemplateView
 
 from store.views import index
 from shop import settings
@@ -13,6 +14,7 @@ urlpatterns = [
     path('account/', include('accounts.urls')),
     path('boutique/', include('store.urls')),
     path('i18n/', include('django.conf.urls.i18n')),  
+    path('sitemap.xml', TemplateView.as_view(template_name="sitemap.xml", content_type="application/xml")),
     path("__reload__/", include("django_browser_reload.urls")),
 ]
 
